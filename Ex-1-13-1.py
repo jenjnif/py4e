77a97dbbff19872394ac5279d292782ac12ab887
jenjnif <jenjnif@hotmail.com>
2018-01-26 22:56:08 +0000

    adding exercises to new repository

---
+++ Ex-1-13-1.py
@@ -0,0 +1,5 @@
+print'1.13 Exercises'
+print'Exercise 1: What is the function of the secondary memory in a computer?'
+print'a) Execute all of the computation and logic of the program'
+print'b) Retrieve web pages over the Internet'
+print'c) Store information for the long term, even beyond a power cycle d) Take input from the user'
